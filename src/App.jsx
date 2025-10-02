dcc24751ca6b94a951558fb63272c650c1031fd0
Thales Oliveira <sr.thales500@gmail.com>
2025-10-02 14:28:14 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <main>
+      <header>
+        <img src="/logo.png" alt="Logo TecBoard" />
+      </header>
+
+      <section>
+        <img src="/banner.png" alt="Banner TecBoard" />
+      </section>
+
+      
+    </main>
+  )
+}
+
+export default App
