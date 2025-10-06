bdbc89f22f8710ae3a86fcbef96a0dadf12a1953
Thales Oliveira <TOliveiraDev@outlook.com>
2025-10-05 22:28:14 -0300

    feat: update App component to include Tema and Banner components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
 import './App.css'
-import {FormularioEvento} from './components/FormularioEvento'
+import { FormularioEvento } from './components/FormularioEvento'
+import { Tema } from './components/Tema'
+import { Banner } from './components/Banner'
 
 function App() {
+
+  const temas = [
+    {
+      id: 1,
+      nome: 'front-end'
+    },
+    {
+      id: 2,
+      nome: 'back-end'
+    },
+    {
+      id: 3,
+      nome: 'devops'
+    },
+    {
+      id: 4,
+      nome: 'inteligência artificial'
+    },
+    {
+      id: 5,
+      nome: 'data science'
+    },
+    {
+      id: 6,
+      nome: 'cloud'
+    }
+  ]
+
   return (
     <main>
       <header>
         <img src="/logo.png" alt="Logo TecBoard" />
       </header>
-      <section>
-        <img src="/banner.png" alt="Banner TecBoard" />
-      </section>
+      <Banner />
       <FormularioEvento />
+
+      {temas.map(function (item) {
+        return (
+          <section key={item.id}>
+            <Tema tema={item} />
+          </section>
+        )
+      })}
     </main>
   )
 }
