ec3f8b6e88f40ded964188fc0d8ef702bef2b357
Thales Oliveira <TOliveiraDev@outlook.com>
2025-10-06 21:53:32 -0300

    fix: add className to option elements in ListaSuspensa for styling consistency

--- src/components/ListaSuspensa/index.jsx
+++ src/components/ListaSuspensa/index.jsx
@@ -3,11 +3,11 @@ import './lista-suspensa.estilos.css'
 export function ListaSuspensa ({ itens, ...rest }){
     return (
         <select {...rest} className='lista-suspensa-form' defaultValue="">
-            <option value="" disabled>
+            <option value="" disabled className='lista-suspensa__options'>
                 Selecione uma opção
             </option>
             {itens.map(function (item) {
-                return <option key={item.id} value={item.id}>
+                return <option className='lista-suspensa__options' key={item.id} value={item.id}>
                     {item.nome}
                 </option>
             })}
